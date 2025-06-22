1a90ebea44ab9ef6bb57397274ec3f1b9c885531
sofiavitoriaa <sofiavitoria4331@gmail.com>
2025-06-22 10:51:18 -0300

    Criando o projeto

---
+++ concat_dados.py
@@ -0,0 +1,8 @@
+# Receba dois dados diferentes do usuário e concatene-o em uma única string.
+
+info1 = input("Digite a primeira informação: ")
+info2 = input("Digite a segunda informação: ")
+
+info_concatenada = info1 + " " + info2
+
+print("As informações concatenadas são: ", info_concatenada)
